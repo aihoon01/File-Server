f663b301a32282d5eaeb52922de27219c888ce38
Stephen Paakow Aihoon <stephen.aihoon@amalitech.org>
2022-06-29 19:42:44 +0000

    changed cors origin

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin:'https://fileserva.herokuapp.com/, https://file-servar.herokuapp.com/index.html'
+    origin: process.env.URL || '*'
 }));
 
 // app.use(cors());
